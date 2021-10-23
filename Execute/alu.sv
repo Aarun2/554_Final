3e465770172e3ea01a48223f913c7945ce4780a7
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-10-23 00:00:12 -0500

    Add files via upload

--- Execute/alu.sv
+++ Execute/alu.sv
@@ -1,17 +1,20 @@
-module alu(a, b, alu_op, result);
+module alu(a, b, alu_op, result, branch);
     input signed [31:0] a, b;
     input [3:0] alu_op;
-    output reg signed [32:0] result;
-    output branch;
+    output logic signed [31:0] result;
+    output logic branch;
+	
+	wire [64:0] mult;
 
-    wire neg_shft;
+	assign mult = a * b;
 
     always @(*)
         case(alu_op[1:0])
             4'b00 : branch = &alu_op[3:2] & ~|(a^b); // beq
             4'b01 : branch = &alu_op[3:2] & |(a^b); // bne
-            4'b10 : branch = (a > b); // a bgt b
-            default : branch = (a < b); // a blt b
+            4'b10 : branch = &alu_op[3:2] & (a > b); // a bgt b
+			4'b11 : branch = &alu_op[3:2] & (a < b); // a blt b
+            default : branch = 0; // don't branch
         endcase
     
     always @(*)
@@ -21,12 +24,13 @@ module alu(a, b, alu_op, result);
             4'b0010 : result = a^b; // a ^ b
             4'b0011 : result = a|b; // a | b
             4'b0100 : result = a&b; // a & b
-            4'b0101 : result = a << b; // a sll b
-            4'b0110 : result = a >> b; // a srl b
-            4'b0111 : result = |(b[31:5]) ? {32{a[31]}} : {{b{a[31]}}, a[31:b]};
-            4'b1000 : result = (a < b) ? : 32'd1 : 32'd0; // set less than 
-            4'b1001 : result = (a * b)[31:0]; // a * b
-            default : result  = b << 12; // load upper immediate
+            4'b0101 : result = |(b[31:5]) ? 32'd0 : a << b; // a sll b
+            4'b0110 : result = |(b[31:5]) ? 32'd0 : a >> b; // a srl b
+            4'b0111 : result = |(b[31:5]) ? {32{a[31]}} : a >>> b; // a sra b
+            4'b1000 : result = (a < b) ? 32'b1 : 32'b0; // set less than 
+            4'b1001 : result = mult[31:0]; // a * b
+			4'b1010 : result = b << 12; // load upper immediate
+            default : result  = {32{1'b1}}; // default all high
         endcase
 
 endmodule
