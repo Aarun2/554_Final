bce5d05b4a6bf01a37e64ce0ab8f6501cff423af
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-10-29 20:57:03 -0500

    Add files via upload

--- writeback/writeback.sv
+++ writeback/writeback.sv
@@ -3,6 +3,6 @@ module writeback(wb_sel, result, mem_data, wb_reg);
   input [31:0] result, mem_data;
   output [31:0] wb_reg;
   
-  assign wb_reg = (wb_sel) ? result : mem_data;
+  assign wb_reg = (wb_sel) ? mem_data : result;
   
 endmodule
