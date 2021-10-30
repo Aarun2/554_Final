395942d16bd151b3b1dbeb6d0b9b9ae771489dc2
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-10-29 20:56:30 -0500

    Add files via upload

--- Decode/control.sv
+++ Decode/control.sv
@@ -1,4 +1,122 @@
-module control();
+module control(op, alu_op, jump, branch, write, imm_sel, wb_sel);
 
+	input [6:0] op;
+	
+	output [3:0] alu_op;
+	output jump, branch, write, imm_sel, wb_sel;
 
+	assign alu_op = (~op[6] & op[5] & ~op[4]) ? 4'h1 : op[3:0];
+	
+	assign jump = op[6] & op[5] & op[4] & op[3] & op[2] & op[1];
+	
+	assign branch = (~op[6] & op[5] & op[4]);
+	
+	assign imm_sel = op[4] ^ op[5];
+	
+	assign wb_sel = (~op[6] & op[5] & ~op[4]);
+	
+	always @(op)
+		casex(op)
+			7'h00 : begin // NOP
+				write = 0;
+			end
+			7'h01 : begin // add
+				write = 1;
+			end
+			7'h02 : begin // sub
+				write = 1;
+			end
+			7'h03 : begin // xor
+				write = 1;
+			end
+			7'h04 : begin // or
+				write = 1;
+			end
+			7'h05 : begin // and
+				write = 1;
+			end
+			7'h06 : begin // sll
+				write = 1;
+			end
+			7'h07 : begin // srl
+				write = 1;
+			end
+			7'h08 : begin // sra
+				write = 1;
+			end
+			7'h09 : begin // slt
+				write = 1;
+			end
+			7'h0A : begin // mul
+				write = 1;
+			end
+			7'h11 : begin // addi
+				write = 1;
+			end
+			7'h13 : begin // xori
+				write = 1;
+			end
+			7'h14 : begin // ori
+				write = 1;
+			end
+			7'h15 : begin // andi
+				write = 1;
+			end
+			7'h16 : begin // slli
+				write = 1;
+			end
+			7'h17 : begin // srli
+				write = 1;
+			end
+			7'h18 : begin // srai
+				write = 1;
+			end
+			7'h19 : begin // slti
+				write = 1;
+			end
+			7'h1B : begin // lui
+				write = 1;
+			end
+			7'h20 : begin // lw
+				write = 1;
+			end
+			7'h21 : begin // sw
+				write = 0;
+			end
+			7'h3C : begin // beq
+				write = 0;
+			end
+			7'h3D : begin // bne
+				write = 0;
+			end
+			7'h3E : begin // blt
+				write = 0;
+			end
+			7'h3F : begin // bgt
+				write = 0;
+			end
+			7'h50 : begin // matmul
+				write = 0;
+			end
+			7'h51 : begin // lam
+				write = 0;
+			end
+			7'h52 : begin // lbm
+				write = 0;
+			end
+			7'h53 : begin // lacc
+				write = 0;
+			end
+			7'h54 : begin // racc
+				write = 0;
+			end
+			7'h7E : begin // j
+				write = 0;
+			end
+			7'h7F : begin // jr
+				write = 0;
+			end
+			default : begin
+				write = 0;
+			end
 endmodule
--- Decode/decode.sv
+++ Decode/decode.sv
@@ -1,13 +1,23 @@
-module decode(clk, rst_n, read1data, read2data, imm_ext);
+module decode(clk, rst_n, writedata, read1data, read2data, imm_ext, alu_op, jump, branch, imm_sel, wb_sel);
 
 	input clk, rst_n;
-	input [31:0] instr;
+	input [31:0] instr, writedata;
 	
 	output [31:0] read1data, read2data, imm_ext;
+	output [3:0] alu_op;
+	output jump, branch, imm_sel, wb_sel;
 	
-	rf reg_file (.clk(clk), .rst_n(rst_n), .read1regsel(instr[19:15]), .read2regsel(), write, writeregsel, writedata, 
-	             .read1data(read1data), .read2data(read2data));
+	wire write;
+	wire [14:0] imm;
+	
+	rf reg_file (.clk(clk), .rst_n(rst_n), .read1regsel(instr[19:15]), .read2regsel(instr[14:10]), .write(write), 
+			     .writeregsel(instr[24:20]), .writedata(writedata), .read1data(read1data), .read2data(read2data));
 				 
-	extend_15 extension (.imm(instr[14:0]), .imm_ext(imm_ext));
+	control control_inst (.op(instr[31:25]), .alu_op(alu_op), .jump(jump), .branch(branch), .write(write), .imm_sel(imm_sel)
+						  .wb_sel(wb_sel));
+	
+	assign imm = (branch) ? {instr[24:20], instr[9:0]} : instr[14:0];
+				 
+	extend_15 extension (.imm(imm), .imm_ext(imm_ext));
 
 endmodule
