4c56c1330c504fe44e84b235866c6dc3237de38c
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-10-31 15:21:10 -0500

    Add files via upload

--- proc.sv
+++ proc.sv
@@ -4,10 +4,11 @@ module proc(clk, rst_n);
 	
 	wire [31:0] read1data, read2data, imm, result, writedata;
 	wire [3:0] alu_op;
-	wire jump, branch_instr, imm_sel, branch_dec, wb_sel;
+	wire imm_sel, branch_dec, wb_sel;
+	wire [1:0] branch_instr;
 	
 	decode decode_proc(.clk(clk), .rst_n(rst_n), .writedata(writedata), .read1data(read1data), .read2data(read2data), .imm_ext(imm), 
-		               .alu_op(alu_op), .jump(jump), .branch(branch_instr), .imm_sel(imm_sel), .wb_sel(wb_sel));
+		               .alu_op(alu_op), .branch(branch_instr), .imm_sel(imm_sel), .wb_sel(wb_sel));
 	
 	execute execute_proc(.read1data(read1data), .read2data(read2data), .imm(imm), .alu_op(alu_op), .imm_sel(imm_sel), 
 				         .result(result), .branch(branch_dec));
