bbb841af4404c56b3fc3307e481e66e846ce5df2
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-11-02 18:23:55 -0500

    Add files via upload

--- writeback/writeback.sv
+++ writeback/writeback.sv
@@ -1,8 +1,24 @@
-module writeback(wb_sel, result, mem_data, wb_reg);
-  input wb_sel;
+module writeback(clk, rst_n, flush, wb_sel, write_in, result, mem_data, wb_reg, write);
+  
+  input clk, rst_n, flush, wb_sel, write_in;
   input [31:0] result, mem_data;
-  output [31:0] wb_reg;
   
-  assign wb_reg = (wb_sel) ? mem_data : result;
+  output logic [31:0] wb_reg;
+  output logic write;
   
+  wire [31:0] wb_reg_in;
+  
+  assign wb_reg_in = (wb_sel) ? mem_data : result;
+  
+  always_ff @(posedge clk)
+	wb_reg <= wb_reg_in;
+
+  always_ff @(posedge clk, negedge rst_n)
+	if (!rst_n)
+		write <= 0;
+	else if (flush)
+		write <= 0;
+	else
+		write <= write_in;
+
 endmodule
