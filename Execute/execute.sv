44dd6b3192be513a601bf191249966c07c07e96b
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-11-09 13:08:17 -0600

    Add files via upload

--- Execute/execute.sv
+++ Execute/execute.sv
@@ -1,45 +1,47 @@
-module execute(clk, rst_n, flush, writeregsel_d, read1data, read2data, imm, alu_op, imm_sel, wb_sel_d, write_d, branch_d, result, branch, 
-               branch_dec, wb_sel, write, writeregsel);
-
+module execute #(parameter PC_BITS = 16) (clk, rst_n, flush, writeregsel_d, read1data, read2data, imm, alu_op, pc_d, imm_sel,
+                                          wb_sel_d, write_d, m_write_d, branch, result, wb_sel, write, m_write, writeregsel, pc);
+			   
 	input clk, rst_n, flush;
 	input [31:0] read1data, read2data, imm;
 	input [3:0] alu_op;
-	input imm_sel, wb_sel_d, write_d;
-	input [1:0] branch_d;
+	input imm_sel, wb_sel_d, write_d, m_write_d;
+	input [1:0] branch;
 	input [4:0] writeregsel_d;
+	input [PC_BITS-1:0] pc_d;
 	
 	output logic [31:0] result;
-	output logic branch_dec, wb_sel, write;
-	output logic [1:0] branch;
+	output logic wb_sel, write, m_write;
 	output logic [4:0] writeregsel;
+	output logic [PC_BITS-1:0] pc;
 	
 	wire [31:0] b, result_d;
-	wire branch_dec_d;
+	wire [PC_BITS-1:0] pc_out;
+	wire branch_dec;
 	
 	assign b = (imm_sel) ? imm : read2data; 
 	
-	alu alu_DUT (.a(read1data), .b(b), .alu_op(alu_op), .result(result_d), .branch(branch_dec_d));
+	alu i_alu (.a(read1data), .b(b), .alu_op(alu_op), .result(result_d), .branch(branch_dec));
+	
+	branch_pc #(.PC_BITS(PC_BITS)) br_pc (.pc_in(pc_d), .imm(imm), .read1data(read1data), .branch_dec(branch_dec), 
+	                                      .branch(branch), .pc(pc_out));
 	
 	//////////////////
 	// EX/MEM Flops //
 	//////////////////
 	
+	always_ff @(posedge clk, negedge rst_n)
+		if (!rst_n)
+			pc <= 0;
+		else if (flush)
+			pc <= pc_d;
+		else
+			pc <= pc_out;
+			
 	always_ff @(posedge clk)
 		writeregsel <=  writeregsel_d;
 	
 	always_ff @(posedge clk)
 		result <= result_d;
-		
-	always_ff @(posedge clk, negedge rst_n)
-		if (!rst_n)
-			branch <= 0;
-		else if (flush)
-			branch <= 0;
-		else
-			branch <= branch_d;
-	
-	always_ff @(posedge clk)
-		branch_dec <= branch_dec_d;
 			
 	always_ff @(posedge clk, negedge rst_n)
 		if (!rst_n)
@@ -48,6 +50,14 @@ module execute(clk, rst_n, flush, writeregsel_d, read1data, read2data, imm, alu_
 			write <= 0;
 		else
 			write <= write_d;
+	
+	always_ff @(posedge clk, negedge rst_n)
+		if (!rst_n)
+			m_write <= 0;
+		else if (flush)
+			m_write <= 0;
+		else
+			m_write <= m_write_d;
 	
 	always_ff @(posedge clk)
 		wb_sel <= wb_sel_d;
