5cf7eef925f4d3537c6fcadf05399339ad6548aa
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-11-09 10:36:32 -0600

    Add files via upload

--- Execute/execute.sv
+++ Execute/execute.sv
@@ -1,23 +1,34 @@
-module execute(clk, rst_n, flush, read1data, read2data, imm, alu_op, imm_sel, wb_sel_in, write_in, branch_in, result, branch, 
-               branch_dec, wb_sel, write);
+module execute(clk, rst_n, flush, writeregsel_d, read1data, read2data, imm, alu_op, imm_sel, wb_sel_d, write_d, branch_d, result, branch, 
+               branch_dec, wb_sel, write, writeregsel);
 
 	input clk, rst_n, flush;
 	input [31:0] read1data, read2data, imm;
 	input [3:0] alu_op;
-	input imm_sel, wb_sel_in, write_in, branch_in;
+	input imm_sel, wb_sel_d, write_d;
+	input [1:0] branch_d;
+	input [4:0] writeregsel_d;
 	
 	output logic [31:0] result;
-	output logic branch, branch_dec, wb_sel, write;
+	output logic branch_dec, wb_sel, write;
+	output logic [1:0] branch;
+	output logic [4:0] writeregsel;
 	
-	wire [31:0] b, result_in;
-	wire branch_dec_in;
+	wire [31:0] b, result_d;
+	wire branch_dec_d;
 	
 	assign b = (imm_sel) ? imm : read2data; 
 	
-	alu alu_DUT (.a(read1data), .b(b), .alu_op(alu_op), .result(result_in), .branch(branch_dec_in));
+	alu alu_DUT (.a(read1data), .b(b), .alu_op(alu_op), .result(result_d), .branch(branch_dec_d));
+	
+	//////////////////
+	// EX/MEM Flops //
+	//////////////////
 	
 	always_ff @(posedge clk)
-		result <= result_in;
+		writeregsel <=  writeregsel_d;
+	
+	always_ff @(posedge clk)
+		result <= result_d;
 		
 	always_ff @(posedge clk, negedge rst_n)
 		if (!rst_n)
@@ -25,10 +36,10 @@ module execute(clk, rst_n, flush, read1data, read2data, imm, alu_op, imm_sel, wb
 		else if (flush)
 			branch <= 0;
 		else
-			branch <= branch_in;
+			branch <= branch_d;
 	
 	always_ff @(posedge clk)
-		branch_dec <= branch_dec_in;
+		branch_dec <= branch_dec_d;
 			
 	always_ff @(posedge clk, negedge rst_n)
 		if (!rst_n)
@@ -36,9 +47,9 @@ module execute(clk, rst_n, flush, read1data, read2data, imm, alu_op, imm_sel, wb
 		else if (flush)
 			write <= 0;
 		else
-			write <= write_in;
+			write <= write_d;
 	
 	always_ff @(posedge clk)
-		wb_sel <= wb_sel_in;
+		wb_sel <= wb_sel_d;
 
 endmodule
