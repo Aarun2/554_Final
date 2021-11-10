9295d5bd725a38bc70d385e9b937ef5147a2a492
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-11-10 14:10:36 -0600

    Add files via upload

--- Testing/reg_test.py
+++ Testing/reg_test.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python3
 
+import subprocess;
+
+def ctrl_tb():
+    subprocess.run("vlog -work work control.sv control_tb.sv", shell = True)
+    subprocess.run("vsim -c work.control_tb -do run.do", shell = True)
+
+def ext_tb():
+    subprocess.run("vlog -work work extend_15.sv extend_tb.sv", shell = True)
+    subprocess.run("vsim -c work.extend_tb -do run.do", shell = True)
+
+def rf_tb():
+    subprocess.run("vlog -work work rf.sv rf_tb.sv", shell = True)
+    subprocess.run("vsim -c work.rf_tb -do run.do", shell = True)
+    
+if __name__ == '__main__':
+    ctrl_tb()
+    ext_tb()
+    rf_tb()
+    
