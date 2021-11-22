c3aa1f850f53ca8cf604dd17ee3d6ea5d9e931f4
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-11-22 00:42:12 -0600

    Can now calculate Coverage

--- Testing/reg_test.py
+++ Testing/reg_test.py
@@ -3,16 +3,23 @@
 import subprocess;
 
 def ctrl_tb():
-    subprocess.run("vlog -work work control.sv control_tb.sv", shell = True)
-    subprocess.run("vsim -c work.control_tb -do run.do", shell = True)
+    subprocess.run("vlog -coveropt 3 +cover +acc control.sv control_tb.sv", shell = True)
+    subprocess.run('vsim -coverage -vopt work.control_tb -c -do "coverage save -onexit -directive -codeAll control.ucdb; run -all; exit"', shell = True)
+    subprocess.run('vsim -c -do "vcover report -details -html control.ucdb; exit"', shell = True)
+    subprocess.run('mv ./covhtmlreport ./ctrl_coverage', shell =  True)
 
 def ext_tb():
-    subprocess.run("vlog -work work extend_15.sv extend_tb.sv", shell = True)
-    subprocess.run("vsim -c work.extend_tb -do run.do", shell = True)
+    subprocess.run("vlog -coveropt 3 +cover +acc extend_15.sv extend_tb.sv", shell = True)
+    subprocess.run('vsim -coverage -vopt work.extend_tb -c -do "coverage save -onexit -directive -codeAll extend.ucdb; run -all; exit"', shell = True)
+    subprocess.run('vsim -c -do "vcover report -details -html extend.ucdb; exit"', shell = True)
+    subprocess.run('mv ./covhtmlreport ./ext_coverage', shell =  True)
 
 def rf_tb():
-    subprocess.run("vlog -work work rf.sv rf_tb.sv", shell = True)
-    subprocess.run("vsim -c work.rf_tb -do run.do", shell = True)
+    subprocess.run("vlog -coveropt 3 +cover +acc rf.sv rf_tb.sv", shell = True)
+    subprocess.run('vsim -coverage -vopt work.rf_tb -c -do "coverage save -onexit -directive -codeAll rf.ucdb; run -all"', shell = True)
+    subprocess.run('vsim -c -do "vcover report -details -html rf.ucdb; exit"', shell = True)
+    subprocess.run('mv ./covhtmlreport ./rf_coverage', shell =  True)
+
     
 if __name__ == '__main__':
     ctrl_tb()
