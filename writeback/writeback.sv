84fb995044441cf5205368d14e96215966518708
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-12-02 16:37:41 -0600

    Add files via upload

--- writeback/writeback.sv
+++ writeback/writeback.sv
@@ -1,19 +1,18 @@
 module writeback
   (
-  input flush_i, stall_i, reg_wren_i, forward_en_i,
+  input flush_i, stall_i, reg_write_enable_i, forward_en_i,
   input [1:0] wb_sel_i,
   input [31:0] result_i, mem_data_i, cout_i, forward_data_i,
-  output [31:0] reg_wrdata_o,
-  output reg_wren_o,
-  output [4:0] w_dest_reg_o
-   ); 
+  output [31:0] reg_write_data_o,
+  output reg_write_enable_o
+  ); 
 
   logic [31:0] result_d;
   
-  assign reg_wren_o =  flush_i ? 0 : reg_wren_i;
+  assign reg_write_enable_o =  flush_i ? 0 : reg_write_enable_i;
   
   assign result_d = (forward_en_i) ? forward_data_i : result_i;
-  
-  assign reg_wrdata_o = (wb_sel_i == 2'b00) ? result_i : (wb_sel_i == 2'b01) ? mem_data_i : (wb_sel_i == 2'b10) ? cout_i : result_d;
+    
+  assign reg_write_data_o = (wb_sel_i == 2'b00) ? result_i : (wb_sel_i == 2'b01) ? mem_data_i : (wb_sel_i == 2'b10) ? cout_i : result_d;
 
 endmodule
